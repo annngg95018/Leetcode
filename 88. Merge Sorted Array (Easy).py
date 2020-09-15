7a6d0d8a523bb1ccde189ad89c19362d3351d975
annngg95018 <anngg95018@gmail.com>
2020-09-15 18:16:44 +0800

    Update 88. Merge Sorted Array (Easy).py

--- 88. Merge Sorted Array (Easy).py
+++ 88. Merge Sorted Array (Easy).py
@@ -20,8 +20,8 @@ pointer1 = m-1
    pointer2 = n -1
         pointer3 = m + n -1
         
-        while pointer1 >= 0 and pointer2 >= 0:
-            if nums1[pointer1] >= nums2[pointer2]:
+        while pointer1 >= 0 and pointer2 >= 0: //注意要有等號
+            if nums1[pointer1] >= nums2[pointer2]: // 注意要有等號不然程式會crash
                 nums1[pointer3] = nums1[pointer1]
                 pointer1 -= 1
                 pointer3 -= 1
