4c494b841ff1fd08d32484e969002de891dea47c
annngg95018 <anngg95018@gmail.com>
2021-05-12 03:06:04 +0800

    Update 680. Valid Palindrome II (Easy).py

--- 680. Valid Palindrome II (Easy).py
+++ 680. Valid Palindrome II (Easy).py
@@ -6,7 +6,7 @@ class Solution:
                 i += 1
                 j -= 1
             else:
-                x = s[i:j]
+                x = s[i:j] //範圍是i~j-1
                 y = s[i+1:j+1]
                 return True if x[::-1] == x or y[::-1] == y  else False //[::-1] 倒敘
             
